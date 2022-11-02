78c5907ccb13609ef1bf58bd2de9fad7a42a8fba
Israel Mello <charrels22@outlook.com>
2022-11-02 18:49:13 -0300

    estrutura inicial do projeto

---
+++ src/components/pages/Login/login.jsx
@@ -0,0 +1,15 @@
+import style from './login.module.css';
+
+const Home = () => {
+    return (
+        <div className={style.Wrapper}>
+            <div className={style.TitleWrapper}>
+                <div className={style.Title}>
+                    Login Page
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
